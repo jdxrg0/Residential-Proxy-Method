857445198fb1795a4b2b92924c014eeeddab9add
John David Garcia <253022350+jdxrg0@users.noreply.github.com>
2026-09-22 22:59:24 +0800

    Add puppeteer stealth plugin

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const puppeteer = require('puppeteer');
-
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+puppeteer.use(StealthPlugin());
 const targetUrl = process.env.CHAT_URL;
 const targetMessage = process.env.CHAT_MESSAGE;
 const codeMessage = process.env.CHAT_CODE_MESSAGE;
